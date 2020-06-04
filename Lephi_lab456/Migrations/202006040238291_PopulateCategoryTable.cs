c7fda14b81caff76b2608be2e3dda5da0ab71652
Administrator <Administrator@PC227>
2020-06-04 09:39:42 +0700

    1

---
+++ Lephi_lab456/Migrations/202006040238291_PopulateCategoryTable.cs
@@ -0,0 +1,17 @@
+namespace Lephi_lab456.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulateCategoryTable : DbMigration
+    {
+        public override void Up()
+        {
+
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
