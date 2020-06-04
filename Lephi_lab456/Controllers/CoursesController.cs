866c3d4b9f08c61edd0e28ef93bc61d86483fa8b
Administrator <Administrator@PC227>
2020-06-04 11:14:11 +0700

    1234

---
+++ Lephi_lab456/Controllers/CoursesController.cs
@@ -0,0 +1,38 @@
+﻿using Lephi_lab456.Models;
+using Lephi_lab456.ViewModels;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lephi_lab456.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        // GET: Courses
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(CourseViewModel viewModel)
+        {
+            var course = new Course
+            {
+                LecturerId = User.Identity.GetUserId(),
+                DateTime = viewModel.GetDateTime(),
+                CategoryId = viewModel.Category,
+                Place = viewModel.Place
+            
+            };
+            _dbContext.Courses.Add(course);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
--- Lephi_lab456/Migrations/202006040238291_PopulateCategoryTable.cs
+++ Lephi_lab456/Migrations/202006040238291_PopulateCategoryTable.cs
@@ -7,7 +7,9 @@ namespace Lephi_lab456.Migrations
     {
         public override void Up()
         {
-
+           Sql("INSERT INTO CATEGORIES (ID, NAME) VALUES (1, 'Development')");
+           Sql("INSERT INTO CATEGORIES (ID, NAME) VALUES (2, 'Business')");
+           Sql("INSERT INTO CATEGORIES (ID, NAME) VALUES (3, 'Marketing')"); 
         }
         
         public override void Down()
