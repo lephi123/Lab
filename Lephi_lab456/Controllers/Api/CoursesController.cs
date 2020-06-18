33f13209eb4de2656b30c34c4c1d37f5cebaf62c
Administrator <Administrator@PC276>
2020-06-18 10:36:47 +0700

    aaa

---
+++ Lephi_lab456/Controllers/Api/CoursesController.cs
@@ -0,0 +1,20 @@
+﻿using Lephi_lab456.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Lephi_lab456.Controllers.Api
+{
+    public class CoursesController : ApiController
+    {
+        public ApplicationDbContext _dbContext { get; set; }
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        [HttpDelete]
+    }
+}
